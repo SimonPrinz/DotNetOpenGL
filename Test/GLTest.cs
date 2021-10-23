51a9d3f41ed66553d60d84aa0cf105de05bebe86
Simon Prinz <Hey@SimonPri.nz>
2021-10-24 00:39:34 +0200

    add more tests

---
+++ Test/GLTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Test
+{
+	public class GLTest
+	{
+		public static long NanoTime()
+		{
+			long lNano = 10000L * Stopwatch.GetTimestamp();
+			lNano /= TimeSpan.TicksPerMillisecond;
+			lNano *= 100L;
+			return lNano;
+		}
+		
+		public const int COLOR_BUFFER_BIT = 0x4000;
+
+		#region glViewport
+
+		[DllImport(GL.LIB)]
+		private static extern void glViewport(int pPosX, int pPosY, int pWidth, int pHeight);
+
+		public static void SetViewport(int pPosX, int pPosY, int pWidth, int pHeight) =>
+			glViewport(pPosX, pPosY, pWidth, pHeight);
+
+		#endregion
+
+		#region glClearColor
+
+		[DllImport(GL.LIB)]
+		private static extern void glClearColor(float pRed, float pGreen, float pBlue, float pAlpha);
+
+		public static void SetClearColor(float pRed, float pGreen, float pBlue, float pAlpha) =>
+			glClearColor(pRed, pGreen, pBlue, pAlpha);
+
+		#endregion
+
+		#region glClear
+
+		[DllImport(GL.LIB)]
+		private static extern void glClear(int pMask);
+
+		public static void Clear(int pMask) =>
+			glClear(pMask);
+
+		#endregion
+	}
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,88 +1,22 @@
 ﻿using System;
-using System.Runtime.InteropServices;
-using static GLFW;
 
 namespace Test
 {
-	internal static class Program
+	internal class Program
 	{
-		private const int OPENGL_CORE_PROFILE = 0x32001;
-		private static readonly IntPtr NULL = IntPtr.Zero;
-		
 		private static void Main(string[] pArgs)
 		{
-			if (!Init())
-				throw new InvalidOperationException("could not initialize glfw");
-
+			Game? lGame = null;
 			try
 			{
-				SetWindowHint(CONTEXT_VERSION_MAJOR, 3);
-				SetWindowHint(CONTEXT_VERSION_MINOR, 3);
-				SetWindowHint(OPENGL_PROFILE, OPENGL_CORE_PROFILE);
-
-				Window lWindow = CreateWindow(1280, 720, "DotNetOpenGL", (Monitor) NULL, (Window) NULL);
-				if (lWindow == NULL)
-					throw new InvalidOperationException("could not create glfw window");
-				
-				MakeContextCurrent(lWindow);
-
-				SetKeyCallback(lWindow, (_, pKey, _, _, _) =>
-				{
-					if (pKey == KEY_ESCAPE)
-						SetWindowShouldClose(lWindow, true);
-				});
-				
-				SetViewport(0, 0, 1280, 720);
-				SetClearColor(.2f, .3f, .3f, 1f);
-
-				while (!ShouldWindowClose(lWindow))
-				{
-					Clear(COLOR_BUFFER_BIT);
-					
-					SwapBuffers(lWindow);
-					PollEvents();
-				}
+				lGame = new Game();
+				lGame.Run();
 			}
-			finally
+			catch (Exception lException)
 			{
-				Terminate();
+				Console.WriteLine(lException);
+				lGame?.Dispose();
 			}
 		}
-		
-		#region Test
-
-		public const int COLOR_BUFFER_BIT = 0x4000;
-
-		#region glViewport
-
-		[DllImport(GL.LIB)]
-		private static extern void glViewport(int pPosX, int pPosY, int pWidth, int pHeight);
-
-		public static void SetViewport(int pPosX, int pPosY, int pWidth, int pHeight) =>
-			glViewport(pPosX, pPosY, pWidth, pHeight);
-
-		#endregion
-
-		#region glClearColor
-
-		[DllImport(GL.LIB)]
-		private static extern void glClearColor(float pRed, float pGreen, float pBlue, float pAlpha);
-
-		public static void SetClearColor(float pRed, float pGreen, float pBlue, float pAlpha) =>
-			glClearColor(pRed, pGreen, pBlue, pAlpha);
-
-		#endregion
-
-		#region glClear
-
-		[DllImport(GL.LIB)]
-		private static extern void glClear(int pMask);
-
-		public static void Clear(int pMask) =>
-			glClear(pMask);
-
-		#endregion
-
-		#endregion
 	}
 }
